7a345fd3bbb0b98fda483c51abfe81d4a34464b8
jpcarter34us <jcarterster@gmail.com>
2015-11-05 17:10:32 -0600

    Initial Commit - taeatonDev

---
+++ fundraisers.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+	<!-- Title | Changeable -->
+	<title>Fundraiser</title>
+	<link rel="Stylesheet" type="text/css" href="css/bootstrap.css">
+<!-- 	<link rel="Stylesheet" type="text/css" href="/css/stylesheet1.css"> -->
+	<script src="jquery-1.11.2.min.js"></script>
+	<link rel="icon"
+            type="image/png" 
+            href="img/extra gears.png"></a>
+</head>
+<body class="body">
+	<img src="img/header/small.png" alt="Banner" width="100%">
+	<?php include './base/navBar.html';?>
+	<div class="col-md-6">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">Drive One</h3>
+			</div>
+			<div class="panel-body">
+				Drive One Information
+			</div>
+		</div>
+	</div>
+	<div class="col-md-6">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">Trivia Night</h3>
+			</div>
+			<div class="panel-body">
+				Trivia Night Information
+			</div>
+		</div>
+	</div>
+	<!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+	<!-- Title | Changeable -->
+	<title>Home</title>
+	<link rel="Stylesheet" type="text/css" href="css/bootstrap.css">
+<!-- 	<link rel="Stylesheet" type="text/css" href="/css/stylesheet1.css"> -->
+	<script src="jquery-1.11.2.min.js"></script>
+	<link rel="icon"
+            type="image/png" 
+            href="img/extra gears.png"></a>
+</head>
+<body class="body">
+	<img src="img/header/large.png" alt="Banner" width="100%">
+	<?php include './base/navBar.html';?>
+	<!-- carousel -->
+	<?php include './base/carousel.php';?>
+	
+	<!-- Bacground image Obsolete, but optional -->
+	<!-- <div><img class="rightIMG" src="test.jpg"></div> -->
+	
+	<!-- Right-hand content -->
+	<!-- 
+	<div class="mainDiv" id="right"><img class="divHead" src="img/mission.png"></br><div></br>The Liberty Robotics Foundation, a not for profit
+	501c3, was founded in 2009 by parents of the liberty robotics team promote the expansion of FIRST teams in the Liberty School District</div>
+	</br><div>We are committed to providing funding for the continues growth of all FIRST teams within the Liberty School District. These teams include the FRC Liberty Robotics Teams 1764
+	at the high schools, Tech Challenge, FLL and Jr. FLL teams at the elementary and middle schools.</div></div>
+	-->
+	
+	<!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+</body>
+</html>
